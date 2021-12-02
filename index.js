e026b07f6cccf87b96f11d2d412005a496fca45a
Oreyne Lang <oreynelang14@gmail.com>
2021-12-02 05:33:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//document.getElementById("count-el").innerText = 53; // 1
+
+let count = 0
+console.log(count)
+
+// console.info(myAge)
+let myAge = 32
+console.info(myAge)
+
+let humanDogAge = 7
+let myDogAge = myAge * humanDogAge
+console.info(myDogAge)
+
+let bonusPoints = 50
+bonusPoints += 100
+bonusPoints -= 25
+bonusPoints += 70
+console.log(bonusPoints)
