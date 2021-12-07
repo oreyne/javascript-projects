2a666210be3ee2f6affcdc12a26c5dfd8828f5f1
Oreyne Lang <oreynelang14@gmail.com>
2021-12-07 14:55:18 -0500

    rest of the function

--- js/advanced_part_three.js
+++ js/advanced_part_three.js
@@ -1,54 +1,56 @@
 let list = ['China','Indonesia','Germany', ,'Canada','Camerun','Laos']
 let data = ['3','4','76','22','52','9']
 let numbers = [3,4,76,22,52,9]
+let numbersT = [3,4,76,22,52,9]
 let test = [null, 1, 4, undefined, false, 0, 3,4,76,3,42, 2, 52, 9, 3, 4]//16
 
-//concat, merge several list in one
-console.log('concat');
-const arr1 = [1, 2]
-const arr2 = [3, 4]
-const arr3 = arr1.concat(arr2);
-const arr4 = [[5], [6, 7]]
-const arr5 = arr3.concat(arr4, arr1)
+//copyWithIn, copy a part of a list, inside itself but keep its length
+console.log('copyWithin');
+console.log(numbers.copyWithin(3));
+console.log(numbersT.copyWithin(3,1));
+console.log(test.copyWithin(4, 6, 8));
 
-// every, return true or false, if all values inside the list meet the condition
-console.log('every');
-console.log(numbers.every(item => item > -1));
-console.log(numbers.every(item => item > 4));
+//entries, create an iterator of the list
+console.log('entries');
+const iter = numbers.entries();
+console.log(iter.next());
+for (let e of iter) {
+  console.log(e);
+}
 
-//find, return the first element that satisfies a condition
-console.log('find');
-console.log(test.find((item) => item > 33 ));
-console.log(test.find((item, index) => item > 33 && index > 9 ));
+//filter, return an array of element, that pass a function
+console.log('filter');
+const result = list.filter(word => word.length > 6)
+console.log(result);
 
-//includes, return true or false, if a element exist in a list
-console.log('includes');
-console.log(test.includes(55));
-console.log(test.includes(42));
-console.log(test.includes(42, 12));
+//fill, fill an list with an elemento, from a position to another
+console.log('fill');
+console.log(test.fill(5, 10));
+console.log(test.fill(6, 12, 14));
 
-//indexOf, return first index of a element in a list
-console.log('indexOf');
-console.log(test.indexOf(4));
-console.log(test.indexOf(4, 5));
-console.log(test.indexOf(4, -1));
+//findIndex, find index of first element that pass a function
+console.log('findIndex');
+console.log(numbers.findIndex(item => item > 33));//expected result: 2
 
-//lastIndexOf, find the last index of an element
-console.log('lastIndexOf');
-let lastFind = test.lastIndexOf(4)
-console.log(lastFind)
-lastFind = test.lastIndexOf(34)
-console.log(lastFind)
-lastFind = test.lastIndexOf(3, 11)
-console.log(lastFind)
+// forEach, call a function for each element
+console.log('forEach');
+numbers.forEach(item => console.log(item + 1))
 
-//map, apply the function to all elements in a list
-console.log('map');
-const l1 = numbers.map(item => item + 2)
-console.log(l1);
+//from, create an array from a string, or another list
+console.log('from');
+console.log(Array.from('Hola mundo'));
+console.log(Array.from([1,2,3], item => item + 2));
 
-//slice, extract elements from a list, from an index, to an index, not include the second index
-console.log('slice');
-const newList = test.slice(3, 5)
-console.log(newList);
-// dudas con respecto a los indices negativos
+//splice, add elements and remove element at a position
+console.log('splice');
+let abc = [1,2,4,5,8]
+abc.splice(2,0,3)//insert value 3
+abc.splice(5,1,6,7)//remove a value(8), and insert 6,7
+console.log(abc);
+
+//toString, convert a list to String
+console.log('toString');
+console.log(abc.toString());
+
+// valueOf,
+console.log('valueOf');
